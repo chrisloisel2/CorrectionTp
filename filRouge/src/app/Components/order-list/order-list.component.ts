3ea259c591a205e55aaa773cddcbd1cd21a99fcb
Christopher Loisel <christopher@MacBook-Pro-de-Christopher.local>
2023-12-18 16:33:04 +0100

    model

--- filRouge/src/app/Components/order-list/order-list.component.ts
+++ filRouge/src/app/Components/order-list/order-list.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { OrderService } from '../../Service/order.service';
 import { CommonModule } from '@angular/common';
 import { FetcherService } from '../../Service/fetcher.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './order-list.component.html',
   styleUrl: './order-list.component.css',
 })
 export class OrderListComponent {
   constructor(public order: OrderService) {}
+
+  nom: string = '';
 
   suppOrder(id: number) {
     this.order.deleteOrder(id).subscribe(() => {
